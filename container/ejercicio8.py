deaba103d64e68120de5460fbe1456205d28c931
Bur0n19 <206003971+Bur0n19@users.noreply.github.com>
2026-03-22 00:33:51 -0600

    Programa 8 terminado

--- container/ejercicio8.py
+++ container/ejercicio8.py
@@ -1,3 +1,5 @@
+from json.decoder import NaN
+
 import customtkinter as ctk
 import pandas as pd
 
@@ -5,6 +7,8 @@ class Ejercicio8(ctk.CTkFrame):
     def __init__(self, parent, controlador):
         super().__init__(parent)
         self.controlador = controlador
+        self.listaNumeros = []
+
 
         self.titulo = ctk.CTkLabel(self, text="8. Suma de N números", font=("Arial", 18, "bold"))
         self.titulo.pack(pady=10)
@@ -15,30 +19,38 @@ class Ejercicio8(ctk.CTkFrame):
         self.btnCalcular = ctk.CTkButton(self, text="Guardar Numero", command=self.sumatoria)
         self.btnCalcular.pack(pady=10)
 
+        self.total = ctk.CTkLabel(self, text=f"Suma total: 0", font=("Arial", 18, "bold"))
+        self.total.pack(pady=10)
+
+        self.alertas = ctk.CTkLabel(self, text="", text_color="red")
+        self.alertas.pack(pady=5)
+
         self.caja_tabla = ctk.CTkTextbox(self, width=600, height=200, font=("Courier New", 12))
         self.caja_tabla.pack(pady=10)
 
         self.btn_regresar = ctk.CTkButton(self, text="Volver al Menú",command=lambda: controlador.mostrar_frame("menu"), fg_color="gray")
         self.btn_regresar.pack(pady=10)
 
-        self.listaNumeros = []
 
     def sumatoria(self):
-        numero = float(self.entradaNumero.get())
-        self.listaNumeros.append(numero)
+        try:
+            numero = float(self.entradaNumero.get())
+            self.alertas.configure(text="")
+            self.listaNumeros.append(numero)
+            self.suma = sum(self.listaNumeros)
+            self.total.configure(text=f"Suma total: {self.suma}")
+            self.entradaNumero.delete(0, 'end')
+            self.actualizar_tabla_visual()
+        except ValueError:
+            self.alertas.configure(text="Ingresa un número válido para la suma", text_color="red")
 
-        suma_total = sum(self.listaNumeros)
 
-
-        self.actualizar_tabla_visual()
 
     def actualizar_tabla_visual(self):
         df = pd.DataFrame(self.listaNumeros, columns=["Numero"])
 
         self.caja_tabla.delete("1.0", "end")
         self.caja_tabla.insert("1.0", df.to_string(index=False))
-
-
 
 
 
